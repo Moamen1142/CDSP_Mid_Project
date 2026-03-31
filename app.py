df8d0c49d9cc82b6da22a7c32f93f688bd9fa81a
Moamen Mohamed <103082043+Moamen1142@users.noreply.github.com>
2026-03-31 07:20:04 +0200

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,12 @@ color = ['#006E99', '#003B6F', '#C9A84C', '#F5E6C8', '#FFFFFF']  if selected_pla
 image = 'https://www.logo.wine/a/logo/Disney%2B/Disney%2B-Logo.wine.svg' if selected_platform=='DISNEY' else 'https://www.logo.wine/a/logo/Netflix/Netflix-Logo.wine.svg'
 st.image(image,width=200)
 
-df = pd.read_csv(f'{str(selected_platform).lower()}_cleaned.csv')
+# Load data after platform is selected
+@st.cache_data
+def load_data(selected_platform):
+    return pd.read_csv(f'{selected_platform.lower()}_cleaned.csv')
+
+df = load_data(selected_platform)
 df['date_added'] = pd.to_datetime(df['date_added'])
 df['duration'] = pd.to_numeric(df['duration'])
 selected_type = st.sidebar.selectbox('Type', options=['Movie', 'TV Show'])
